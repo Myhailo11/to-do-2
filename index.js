038e573fb1593d1c8236c68bdd4c78a08e935e16
us5nar <us5nar@gmail.com>
2024-01-31 23:15:28 +0200

    fixed some issues with index.js, added parsing of local storage

--- index.js
+++ index.js
@@ -1,38 +1,68 @@
-const list = document.querySelector(".list");
-const input = document.querySelector(".input");
-const btn = document.querySelector(".btn");
-let tasks=[];
+document.addEventListener("DOMContentLoaded", () => {
 
-const todoHandler = () => {
-  if (input.value.trim()!=="") {
-    const dataText = input.value;
-    const rundomNumber = Math.round(Math.random() * 100000);
-    const task = {
-      id:rundomNumber,
-      text:dataText,
-      completed:false
+  const list = document.querySelector(".list");
+  const input = document.querySelector(".input");
+  const btn = document.querySelector(".btn");
 
-    };
-    tasks.push(task);
-    input.value="";
+ localStorage.setItem("tasks", []);
+  console.log(`Tasks form local Storage: `, localStorage.getItem("tasks"));
+  //If we have no saved tasks (NULL) - set tasks array to empty array!
+  let tasks = localStorage.getItem("tasks") ? localStorage.getItem("tasks") : [];
+  console.log(`Local tasks array: `,tasks);
+
+  console.log(`Initial tasks Array length: `,tasks.length);
+  if (tasks.length != 0) {
+    renderTodo(tasks)
+  } else {
+    console.log(`There are no tasks to render`);
+  };
+//Inint end
+
+
+  console.log();
+
+// Add a new task
+  btn.addEventListener("click", (event) => {
+    event.preventDefault();
+    todoHandler();
+    renderTodo(tasks);
+  });
+
+ //Remove the task 
+  window.removeTodo = function (id) {
+    const filteredTasks = tasks.filter((item) => item.id !== id);
+    tasks = filteredTasks;
+    renderTodo(tasks);
   }
-  console.log(tasks);
-}
+  //Complete the task
+  //Will be here
 
-console.log();
-
-btn.addEventListener("click",(event)=>{
-  event.preventDefault();
-  todoHandler();
-  renderTodo(tasks);
-});
+//Functional section  
+  const todoHandler = () => {
+    if (input.value.trim() !== "") {
+      const dataText = input.value;
+      const rundomNumber = Math.round(Math.random() * 100000);
+      const isCompleted = false;
+      const task = {
+        id: rundomNumber,
+        text: dataText,
+        completed: isCompleted
+      };
+      tasks.push(task);
+      //Saving Array to the local Storage
+      localStorage.setItem("tasks", tasks);
+      //console.log(`Array from Local Storage:`, localStorage.getItem("tasks"));
+      input.value = "";
+    }
+    console.log(tasks);
+  }
 
 
 function renderTodo(items) {
-  const marcup = items.map((item)=>{
+  const marcup = items.map((item) => {
     return `<li>
     <label class="label">
-    <input type="checkbox" class="checkbox" checked=${item.completed}>
+    <input type="checkbox" class="checkbox" ${item.completed}>
     <span class="custom__radio"></span>
     </label>
     <p>${item.text}</p>
@@ -40,11 +70,8 @@ function renderTodo(items) {
     </li>`
   }).join("");
   console.log(marcup);
-  list.innerHTML=marcup;
+  list.innerHTML = marcup;
 }
 
-window.removeTodo=function (id) {
-  const filteredTasks = tasks.filter((item)=>item.id!==id);
-  tasks=filteredTasks;
-  renderTodo(tasks);
-}
+
+})
